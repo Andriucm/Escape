bccde79ec8ed497f18f9ecc0b90529509fcf8d06
Andriucm <andreimihalatxe@gmail.com>
2023-01-10 12:13:55 +0100

    componente footer

---
+++ escaperoom/resources/js/Footer.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+        <footer>
+                <h1>
+                        <strong>Grupo 4</strong> 
+                </h1>
+        </footer>
+</template>
+
+<style>
+
+</style>
