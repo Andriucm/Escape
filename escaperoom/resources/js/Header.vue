05ec0721485eb9c851c6b05f32daa849f19bb784
Andriucm <andreimihalatxe@gmail.com>
2023-01-12 08:03:21 +0100

    --

--- escaperoom/resources/js/Header.vue
+++ escaperoom/resources/js/Header.vue
@@ -9,7 +9,7 @@
         <img  src="../../resources/img/logo.png" alt="Logo" class="logo">
         <ul>
           <li><a href="./puntuaciones">Puntuaciones</a></li>
-          <li><a href=". /groups">Grupos</a></li>
+          <li><a href="./groups">Grupos</a></li>
           <li><a href="./profile">Perfil</a></li>
           <li><a href="./login">Ingresa tu cuenta</a></li>
           <button type="button" class="btn">Jugar</button>
