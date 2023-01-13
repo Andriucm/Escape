3efac3b73c1054753c0f1b0a90a893cb0205295a
Mikel <mikelayala1234@gmail.com>
2023-01-13 13:19:51 +0100

    mejoras

--- escaperoom/resources/views/components/layout.blade.php
+++ escaperoom/resources/views/components/layout.blade.php
@@ -6,18 +6,38 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ URL::asset('css/header.css') }}" />
     <link rel="stylesheet" href="{{ URL::asset('css/footer.css') }}" />
-    <link rel="stylesheet" href="{{ URL::asset('css/index.css') }}" />
-    <link rel="stylesheet" href="{{ URL::asset('css/grupos.css') }}" />
+    <link rel="stylesheet" href="{{ URL::asset('css/general.css') }}" />
+    @switch($title)
+        @case("Inicio")
+            <link rel="stylesheet" href="{{ URL::asset('css/index.css') }}" />
+            @break
+        @case("Puntuaciones")
+            <link rel="stylesheet" href="{{ URL::asset('css/puntuaciones.css') }}" />
+            @break
+        @case("Grupos")
+            <link rel="stylesheet" href="{{ URL::asset('css/grupos.css') }}" />
+            @break
+        @case("Perfil")
+            <link rel="stylesheet" href="{{ URL::asset('css/perfil.css') }}" />
+            @break
+        @case("Partida")
+            <link rel="stylesheet" href="{{ URL::asset('css/partida.css') }}" />
+            @break
+    @endswitch
     <title>{{ $title }}</title>
     @vite('resources/js/app.js')
 </head>
 <body>
-    <div id="headerSection"></div>
+    @if ($title != "Partida")
+        <div id="headerSection"></div>
+    @endif
 
     <div id="mainSection" style="width: 100%; height: 100%;">
         {{ $slot }}
     </div>
 
-    <div id="footerSection"></div>
+    @if ($title != "Partida")
+        <div id="footerSection"></div>
+    @endif
 </body>
 </html>
