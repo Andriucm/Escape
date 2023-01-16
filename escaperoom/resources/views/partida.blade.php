4297fc7e67f16307fb1334f1d0945e78c948765e
Mikel <mikelayala1234@gmail.com>
2023-01-16 11:58:07 +0100

    distribucion partida

--- escaperoom/resources/views/partida.blade.php
+++ escaperoom/resources/views/partida.blade.php
@@ -4,9 +4,10 @@
     </x-slot>
 
     <main>
-
-
-
+        <section class="juego"></section>
+        <section class="pistas"></section>
+        <nav class="navegacion"></nav>
+        <aside class="narrativa"></aside>
     </main>
 
 </x-layout>
