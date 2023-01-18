3710e557bdb493fe62ace253d3c1b3a4acddc42b
Mikel <mikelayala1234@gmail.com>
2023-01-18 11:54:46 +0100

    Componentes carousel, zoom imagen pantalla partida

--- escaperoom/resources/js/app.js
+++ escaperoom/resources/js/app.js
@@ -2,7 +2,11 @@ import './bootstrap';
 import { createApp } from 'vue';
 import Header from './Header.vue';
 import Footer from './Footer.vue';
+import CarouselPista from './Carousel.vue';
 
 createApp(Header).mount('#headerSection');
 
 createApp(Footer).mount('#footerSection');
+
+createApp(CarouselPista).mount('#pistaSection');
+
