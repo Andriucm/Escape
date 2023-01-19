aa4a47f36febf911a9812b4c4534e75fa4bc2e41
Andriucm <andrei.mihalatxe@gmail.com>
2023-01-19 17:41:18 +0100

    --

--- escaperoom/resources/views/layouts/header.blade.php
+++ escaperoom/resources/views/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <header class="header-container">
     <div class="header">
-        <a href="{{ route('index') }}"><img src="../public/imagenes/logo.png" alt="Logo" class="logo" ></a>
+        <a href="{{ route('index') }}"><img src="../public/imagenes/logo.png" alt="Logo" class="logo"></a>
         <div class="row">
             <ul>
                 <li><a href="./puntuaciones">Puntuaciones</a></li>
@@ -9,8 +9,9 @@
 
                     <li><a href="./profile">Perfil</a></li>
                 @endauth
+
                 <li><a href="./management">Usuarios</a></li>
-                
+
                 @guest
                     <li><a href="./login">Ingresa tu cuenta</a></li>
                 @else
@@ -18,7 +19,7 @@
                         @csrf
                         <button id="btn-logout">Logout</button>
                     </form>
-                    
+
                 @endauth
                 <button onclick="location.href='{{ route('partida') }}'" type="button" class="btn">Jugar</button>
             </ul>
