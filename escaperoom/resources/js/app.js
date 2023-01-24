16e45808f3a2e9df88abf1a7f4c0ab3d7d72af3f
sarayuscola5 <77096304+sarayuscola5@users.noreply.github.com>
2023-01-24 13:08:55 +0100

    fallo arreglado

--- escaperoom/resources/js/app.js
+++ escaperoom/resources/js/app.js
@@ -3,8 +3,7 @@ import { createApp } from 'vue';
 import Header from './Header.vue';
 import Footer from './Footer.vue';
 import CarouselPista from './Carousel.vue';
-import 'viewerjs/dist/viewer.css'
-import VueViewer from 'v-viewer'
+
 
 createApp(Header).mount('#headerSection');
 
