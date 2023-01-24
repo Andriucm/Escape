a835e0f7b558616e8bbfb15d2af0dbc961d86f08
Andriucm <andreimihalatxe@gmail.com>
2023-01-24 11:46:46 +0100

    --

--- escaperoom/database/seeders/DatabaseSeeder.php
+++ escaperoom/database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
-        // $this->call(PartidaSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(PartidaSeeder::class);
         $this->call(GrupoSeeder::class);
     }
 }
