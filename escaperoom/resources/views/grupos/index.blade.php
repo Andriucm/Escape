6a2886073c75ca6649eef088982fc5d6dc2cf98d
Andriucm <andreimihalatxe@gmail.com>
2023-01-26 14:09:58 +0100

    --

--- escaperoom/resources/views/grupos/index.blade.php
+++ escaperoom/resources/views/grupos/index.blade.php
@@ -5,7 +5,7 @@
     <main>
         @auth
             @if (Auth::User()->rol != 'alumno')
-                <article id="aticulo" onclick="window.location.href='{{route('grupos.create')  }}'">
+                <article id="aticulo" onclick="window.location.href='{{ route('grupos.create') }}'">
                     <h1>+</h1>
                 </article>
             @endif
@@ -15,6 +15,16 @@
             <article>
                 <h1>{{ $grupo->nombre }}</h1>
                 <b>12 usuarios</b>
+                @auth
+                    @if (Auth::User()->rol != 'alumno')
+                        <form action="{{ route('grupos.destroy', $grupo->codGrupo) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <input type="submit" value="Eliminar">
+                        </form>
+                    @endif
+
+                @endauth
 
             </article>
         @endforeach
